8a09243fbb45c19ae25919841b1cc8261cbb0ee6
sk200005 <swayamkorde2005@gmail.com>
2026-02-08 16:16:11 +0530

    ---

--- 03-colorChanger/src/Color.jsx
+++ 03-colorChanger/src/Color.jsx
@@ -1,10 +1,38 @@
-// export default function App() {
-export default function App() {
+import { useState } from "react";
+
+export default function ChangeColor() {
+  const [bgColor, setBgColor] = useState("bg-red-500");
+
   return (
-    <div className="min-h-screen w-full bg-blue-500">
-      <h1 className="text-white text-3xl p-6">
-        Full Screen Colored
-      </h1>
+    <div className={`w-full min-h-screen duration-300 ${bgColor}`}>
+      
+      {/* Bottom buttons */}
+      <div className="fixed bottom-12 inset-x-0 flex justify-center">
+        <div className="flex gap-6 px-4 py-2 rounded-2xl bg-black">
+          
+          <button
+            onClick={() => setBgColor("bg-red-500")}
+            className="text-white"
+          >
+            Red
+          </button>
+
+          <button
+            onClick={() => setBgColor("bg-green-500")}
+            className="text-white"
+          >
+            Green
+          </button>
+
+          <button
+            onClick={() => setBgColor("bg-orange-500")}
+            className="text-white"
+          >
+            Orange
+          </button>
+
+        </div>
+      </div>
     </div>
   );
 }
