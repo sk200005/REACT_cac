576bfecd8a00a6955975a83874dd012fe0fb970f
sk200005 <swayamkorde2005@gmail.com>
2026-02-08 16:15:09 +0530

    Try Fixing the color changing functionality

---
+++ 03-colorChanger/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ChangeColor from './Color'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+     {/* <h1>Hello</h1> */}
+     <ChangeColor/>
+    </>
+  )
+}
+
+export default App
---
+++ 03-colorChanger/src/Color.jsx
@@ -0,0 +1,10 @@
+// export default function App() {
+export default function App() {
+  return (
+    <div className="min-h-screen w-full bg-blue-500">
+      <h1 className="text-white text-3xl p-6">
+        Full Screen Colored
+      </h1>
+    </div>
+  );
+}
