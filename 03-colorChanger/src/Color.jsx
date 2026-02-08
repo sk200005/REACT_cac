3ad8929c858f788f067103c044b5e3d2c29401ca
sk200005 <swayamkorde2005@gmail.com>
2026-02-08 16:29:20 +0530

    Renameing Files

--- 03-colorChanger/src/Color.jsx
+++ 03-colorChanger/src/Color.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 
 export default function ChangeColor() {
+  // 1. Define the state for the background color
   const [bgColor, setBgColor] = useState("bg-red-500");
 
   return (
+    // 2. Use the state variable in the className to dynamically update the color
     <div className={`w-full min-h-screen duration-300 ${bgColor}`}>
       
-      {/* Bottom buttons */}
       <div className="fixed bottom-12 inset-x-0 flex justify-center">
         <div className="flex gap-6 px-4 py-2 rounded-2xl bg-black">
           
+          {/* 3. Update the state when the button is clicked */}
           <button
             onClick={() => setBgColor("bg-red-500")}
             className="text-white"
@@ -22,13 +24,6 @@ export default function ChangeColor() {
             className="text-white"
           >
             Green
-          </button>
-
-          <button
-            onClick={() => setBgColor("bg-orange-500")}
-            className="text-white"
-          >
-            Orange
           </button>
 
         </div>
